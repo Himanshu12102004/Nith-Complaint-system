84c5ff2b91e1a8f46c6abf10fcbb72b6c8633f3f
Himanshu Gupta <himanshu12102004@gmail.com>
2024-05-04 16:20:18 +0530

    remodified response of nature and subnature

--- src/controllers/admin/getAllNatures.ts
+++ src/controllers/admin/getAllNatures.ts
@@ -8,20 +8,29 @@ import { NatureModel } from '../../models/natureOfComplaintModel';
 const getAllNatures: sync_middleware_type = async_error_handler(
   async (req, res, next) => {
     const all = req.query.all;
-    const formattedNatures: any = {};
+    const formattedSubNatures: any = {};
+    const formattedNatures: { label: string; value: string }[] = [];
     if (all == 'true') {
       const natures = await NatureModel.find({});
       natures.forEach((elem) => {
-        formattedNatures[elem.nature.name] = elem.subNature.map((sub) => {
-          return sub.name;
+        formattedNatures.push({
+          label: elem.nature.name,
+          value: elem.nature.name,
+        });
+        formattedSubNatures[elem.nature.name] = elem.subNature.map((sub) => {
+          return { label: sub.name, value: sub.name };
         });
       });
     } else {
       const natures = await NatureModel.find({ 'nature.isActive': true });
       natures.forEach((elem) => {
-        formattedNatures[elem.nature.name] = elem.subNature
+        formattedNatures.push({
+          label: elem.nature.name,
+          value: elem.nature.name,
+        });
+        formattedSubNatures[elem.nature.name] = elem.subNature
           .map((sub) => {
-            if (sub.isActive) return sub.name;
+            if (sub.isActive) return { label: sub.name, value: sub.name };
             else return false;
           })
           .filter((sub) => {
